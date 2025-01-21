288b6366efd262b873528452c34e3362cf751e0e
Armando Rodriguez <avallin2018@gmail.com>
2025-01-20 21:28:42 -0500

    add Teacher page left side

--- src/app/(dashboard)/list/teachers/[id]/page.tsx
+++ src/app/(dashboard)/list/teachers/[id]/page.tsx
@@ -1,4 +1,6 @@
 import React from 'react'
+import Image from "next/image";
+import BigCalendar from "@/components/BigCalendar";
 
 const SingleTeacherPage = () => {
     return (
@@ -6,17 +8,86 @@ const SingleTeacherPage = () => {
             {/*LEFT*/}
             <div className="w-full xl:w-2/3">
                 {/*TOP*/}
-                <div className="">
+                <div className="flex flex-col lg:flex-row gap-4">
                     {/*USER INFO CARD*/}
-                    <div className=""></div>
+                    <div className="bg-lamaSky py-6 px-4 rounded-md flex-1 flex gap-4">
+                        <div className="w-1/3">
+                            <Image
+                                src="https://images.pexels.com/photos/2888150/pexels-photo-2888150.jpeg?auto=compress&cs=tinysrgb&w=1200"
+                                alt="" width={144} height={144} className="w-36 h-36 rounded-full object-cover"/>
+                        </div>
+                        <div className="w-2/3 flex flex-col justify-between gap-4">
+                            <h1 className="text-xl font-semibold">Amy Bibi</h1>
+                            <p className="text-sm text-gray-500">Hac nunc vivamus nam.</p>
+                            <div className="flex items-center justify-between gap-2 flex-wrap text-xs font-medium">
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center ga2">
+                                    <Image src="/blood.png" alt="" width={14} height={14}/>
+                                    <span>A+</span>
+                                </div>
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center ga2">
+                                    <Image src="/date.png" alt="" width={14} height={14}/>
+                                    <span>January 2025</span>
+                                </div>
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center ga2">
+                                    <Image src="/mail.png" alt="" width={14} height={14}/>
+                                    <span>iglesia@gmail.com</span>
+                                </div>
+                                <div className="w-full md:w-1/3 lg:w-full 2xl:w-1/3 flex items-center ga2">
+                                    <Image src="/phone.png" alt="" width={14} height={14}/>
+                                    <span>+1 233 331</span>
+                                </div>
+                            </div>
+
+                        </div>
+                    </div>
                     {/*SMALL CARDS*/}
-                    <div className=""></div>
+                    <div className="flex flex-1 gap-4 justify-between flex-wrap">
+                        {/*CARD*/}
+                        <div className="w-full bg-white p-4 rounded-md
+                        flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                            <Image className="w-6 h-6" src="/singleAttendance.png" alt="" width={24} height={24}/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">90%</h1>
+                                <span className="text-sm text-gray-400">Attendance</span>
+                            </div>
+                        </div>
+                        {/*CARD*/}
+                        <div className="w-full bg-white p-4 rounded-md
+                        flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                            <Image className="w-6 h-6" src="/singleBranch.png" alt="" width={24} height={24}/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">2</h1>
+                                <span className="text-sm text-gray-400">Branches</span>
+                            </div>
+                        </div>
+                        {/*CARD*/}
+                        <div className="w-full bg-white p-4 rounded-md
+                        flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                            <Image className="w-6 h-6" src="/singleLesson.png" alt="" width={24} height={24}/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">6</h1>
+                                <span className="text-sm text-gray-400">Lessons</span>
+                            </div>
+                        </div>
+                        {/*CARD*/}
+                        <div className="w-full bg-white p-4 rounded-md
+                        flex gap-4 md:w-[48%] xl:w-[45%] 2xl:w-[48%]">
+                            <Image className="w-6 h-6" src="/singleClass.png" alt="" width={24} height={24}/>
+                            <div className="">
+                                <h1 className="text-xl font-semibold">6</h1>
+                                <span className="text-sm text-gray-400">Classes</span>
+                            </div>
+                        </div>
+                    </div>
                 </div>
                 {/*BOTTOM*/}
-                <div className="">Schedule</div>
+                <div className="mt-4 bg-white rounded-md p-4 h-[800px ]">
+                    <h1> Teacher&apos;s Schedule </h1>
+                    <BigCalendar />
+                </div>
             </div>
             {/*RIGHT*/}
-             <div className="w-full xl:w-1/3"></div>
+            <div className="w-full xl:w-1/3"></div>
         </div>
     )
 }
